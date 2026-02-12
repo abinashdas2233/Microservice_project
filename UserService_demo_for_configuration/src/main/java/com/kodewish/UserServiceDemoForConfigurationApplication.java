390fd0e95c8d5a8d31356cc48b6553ba88bd9f1b
abinash Das <abinashdas@abinashs-MacBook-Air.local>
2026-02-12 22:04:48 +0530

    demo congig

---
+++ UserService_demo_for_configuration/src/main/java/com/kodewish/UserServiceDemoForConfigurationApplication.java
@@ -0,0 +1,13 @@
+package com.kodewish;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceDemoForConfigurationApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(UserServiceDemoForConfigurationApplication.class, args);
+	}
+
+}
